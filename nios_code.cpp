c886032c3ff5b21dd6f535dc4297dc30c9493656
woutkolkman <wout.kolkman@hotmail.com>
2020-04-23 16:08:05 +0200

    start ccode

--- nios_code.cpp
+++ nios_code.cpp
@@ -1,2 +1,39 @@
 #include <FreeRTOS.h>
+//#include <Arduino_FreeRTOS.h>
+//#include <event_groups.h>
+//#include <semphr.h>
+//#include <queue.h>
 
+// poorten, te vinden in nios_processor.qsys
+#define adc 				(volatile int *) 0x00009000
+#define bel_fft_project				 (int *) 0x00008000
+
+// stack sizes
+#define STCK_SIZE_TASK1 64
+
+// function prototypes
+void init();
+
+int main()
+{
+	init();
+	
+	xTaskCreate(Task1, "Tsk1", STCK_SIZE_TASK1, NULL, 3, NULL);
+	
+	vTaskStartScheduler();
+	return(0); // never reached
+}
+
+void Task1(void *pvParameters) {
+	EventBits_t getEvents;
+	
+	for (;;) {
+		
+	}
+}
+
+void init() {
+	// ddrd etc
+}
+
+void loop() {} // nodig?
